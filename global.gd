f24c1f4eaef8ed76d512678ae83a18705e3b6183
Ethan <ethanfrancis577@gmail.com>
2024-11-20 16:31:43 -0500

    initial commit

---
+++ global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var player_in_water = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
