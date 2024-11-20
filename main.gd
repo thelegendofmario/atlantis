ccd74c88eee45b2d29efad9694e57365507f3ab5
Ethan <ethanfrancis577@gmail.com>
2024-11-20 16:56:25 -0500

    done with water, woring on physics

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Player/Camera2D/CanvasLayer/DebugLabel.text = str($Player.get_gravity())
--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+const WATER_JUMP_VELOCITY = JUMP_VELOCITY/1.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +13,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept") and Global.player_in_water:
+		velocity.y = WATER_JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
