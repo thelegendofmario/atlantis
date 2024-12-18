a13951565ce1f889605cb26cb1443e16ed35271e
Coachinglake <buddyplush@gmail.com>
2024-12-18 12:41:06 -0500

    feat added diffrent stages of gun based on artifact count

--- gun.gd
+++ gun.gd
@@ -6,6 +6,19 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var Bullet = preload("res://bullet.tscn")
 
+func play_anim(movement):
+	var anim = $AnimatedSprite2D
+	if movement == 1:
+		anim.play("bluestage1")
+	if movement == 2:
+		anim.play("bluestage2")
+	if movement == 3:
+		anim.play("bluestage3")
+	if movement == 4:
+		anim.play("bluestage4")
+	if movement == 5:
+		anim.play("bluestage5")
+
 func shoot():
 	var b = Bullet.instantiate()
 	
@@ -15,6 +28,19 @@ func shoot():
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
+	if Global.player_artifacts == 0:
+		play_anim(1)
+	elif Global.player_artifacts == 1:
+		play_anim(2)
+	elif Global.player_artifacts == 2:
+		play_anim(3)
+	elif Global.player_artifacts == 3:
+		play_anim(4)
+	elif Global.player_artifacts == 4:
+		play_anim(5)
+	
+	
 	look_at(get_global_mouse_position())
 
 	move_and_slide()
