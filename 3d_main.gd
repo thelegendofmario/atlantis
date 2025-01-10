7081c2e0b744b1f645c16127b1bf4c808aeb2148
Coachinglake <buddyplush@gmail.com>
2025-01-10 12:39:14 -0500

    FEAT fixed textures

--- 3d_main.gd
+++ 3d_main.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	$player3d/CanvasLayer/DebugLabel.text = str($player3d.get_gravity(), Global.super_artifacts, Engine.get_frames_per_second())
 	$player3d/CanvasLayer/AirBar.value = Global.air/10
 	$"player3d/CanvasLayer/Stamina Bar".value=$player3d.stamina
-	$"player3d/CanvasLayer/Health Bar".value = Global.player_health
+	$"player3d/CanvasLayer/Health Bar".value = Global.dPlayer_Health/2.55
